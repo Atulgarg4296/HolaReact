f8c7e355f8931b60a155c2431d10e42adc75f6ab
Atul Garg <atulgarg4296@gmail.com>
2023-12-12 16:38:29 +0530

    Initializing demo project HolaReact

---
+++ main.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const heading = React.createElement("h2",{id: "line1"},"NamasteReact");
+        const root = ReactDOM.createRoot(document.getElementById("heading"));
+        root.render(heading);
+        
